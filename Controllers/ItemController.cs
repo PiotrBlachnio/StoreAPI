4940f03737c93fd90a90791ff7de927d6c10d9dc
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-08 18:17:12 +0200

    Create item read dto

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Store.Data;
 
 namespace Items.Controllers {
 
     [Route("/api/items")]
     [ApiController]
     public class ItemsController: ControllerBase {
-        
+        private readonly IStoreRepo _repository;
+        private readonly IMapper _mapper;
+
+        public ItemsController(IStoreRepo repository, IMapper mapper) {
+            _repository = repository;
+            _mapper = mapper;
+        }
     }
+
+    [HttpGet]
+    public ActionResult <IEnumerable
 }
---
+++ Dtos/ItemReadDto.cs
@@ -0,0 +1,9 @@
+namespace Store.Dtos
+{
+    public class ItemReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
