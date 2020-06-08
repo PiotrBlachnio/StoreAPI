dcf6ba7fc0164cf16c54f19a4094a4dbf3eaec71
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-08 17:31:30 +0200

    Create item model

---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models {
+    public class Item {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
